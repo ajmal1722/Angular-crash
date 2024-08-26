f6c7af55f239f8d0c93db07157bbe993b5a902f7
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-08-26 17:28:07 +0530

    basic reusable button

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { ButtonComponent } from '../button/button.component';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
-export class HeaderComponent {
 
+export class HeaderComponent {
+  title: string = 'Task Tracker';
+  content: string = 'Add';
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
 }
