bc6bda4341cb76c1ac5a9878c453c6aef36a8e74
Muhammed Ajmal <ajuajmal1722001@gmail.com>
2024-08-27 13:11:10 +0530

    angular font awesome setup

--- src/app/components/task-item/task-item.component.ts
+++ src/app/components/task-item/task-item.component.ts
@@ -1,13 +1,17 @@
 import { Component, Input } from '@angular/core';
 import { TaskInterface } from '../../data/TaskInterface';
+import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-task-item',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FontAwesomeModule, ],
   templateUrl: './task-item.component.html',
   styleUrl: './task-item.component.css'
 })
 export class TaskItemComponent {
   @Input() task!: TaskInterface
+  faTimes = faTimes;
 }
